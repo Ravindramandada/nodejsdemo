0aa6a3522a8da69e179c7718aea35a7a74664345
Ravindramandada <96417132+Ravindramandada@users.noreply.github.com>
2022-01-12 17:42:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Welcome to UMeGrow Happy NewYear');
+  res.send('Happy New Year 2022');
 });
 app.listen(8000, () => {
   console.log('Listening on port 8000');
