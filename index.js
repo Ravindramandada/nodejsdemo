f44b7ace1071f7295ffc9607231aa1f2020bc02c
Ravindramandada <96417132+Ravindramandada@users.noreply.github.com>
2022-01-12 18:07:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Happy New Year 2022');
+  res.send('Happy New Year');
 });
 app.listen(8000, () => {
   console.log('Listening on port 8000');
